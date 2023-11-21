e4fbc51277b05911d644b216419b494c33ac5560
wataru <c0b22003e0@edu.teu.ac.jp>
2023-11-21 15:45:28 +0900

    練習４

--- OneDrive - Katayanagi Institute/ドキュメント/ProjExD2023/ex01/pygame_image.py
+++ OneDrive - Katayanagi Institute/ドキュメント/ProjExD2023/ex01/pygame_image.py
@@ -9,14 +9,15 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")#練習1
     img1 = pg.image.load("ex01/fig/3.png")#練習2番
     img1 = pg.transform.flip(img1,True,False)
-    img1s = [img1,pg.transform.rotozoom(img1,10,1.0)]
+    img1s = [img1,pg.transform.rotozoom(img1,10,1.0)]#練習3
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])#練習4
-        screen.blit(img1s[1],[300,200])
+        
+        screen.blit(img1s[tmr%2],[300,200])#練習5
         pg.display.update()
         tmr += 1        
         clock.tick(10)
