f4f4b25e76365ffc2d1f9763aab457955016f509
wataru <c0b22003e0@edu.teu.ac.jp>
2023-11-21 16:46:43 +0900

    滑らか

--- OneDrive - Katayanagi Institute/ドキュメント/ProjExD2023/ex01/pygame_image.py
+++ OneDrive - Katayanagi Institute/ドキュメント/ProjExD2023/ex01/pygame_image.py
@@ -1,7 +1,6 @@
 import sys
 import pygame as pg
 
-
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))#画面のサイズ
@@ -9,7 +8,7 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")#練習1
     img1 = pg.image.load("ex01/fig/3.png")#練習2番
     img1 = pg.transform.flip(img1,True,False)
-    img1s = [img1,pg.transform.rotozoom(img1,10,1.0)]#練習3
+    img1s = [img1,pg.transform.rotozoom(img1,6,1.0),pg.transform.rotozoom(img1,10,1.0),pg.transform.rotozoom(img1,4,1.0)]#練習3
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -17,14 +16,18 @@ def main():
         x=(tmr%1600)
         screen.blit(bg_img, [-x, 0])#練習4
         screen.blit(bg_img, [1520-x, 0])
-        screen.blit(img1s[tmr%2],[300,200])#練習5
+        screen.blit(img1s[tmr%200//50],[300,200])#
+        
+
         pg.display.update()
-        tmr += 1        
-        clock.tick(200)
+        tmr += 1  
+
+        clock.tick(300)
 
 
 if __name__ == "__main__":
     pg.init()
     main()
+
     pg.quit()
     sys.exit()
